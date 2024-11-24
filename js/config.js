d2b997457fc0c10e1dacdd63b2fb6393e19074c2
Sanris7 <115407290+Sanris7@users.noreply.github.com>
2024-11-24 21:41:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_YF89AJICs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5cm7kdvmlh86elncmrsoucltae', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_jmRRPAqWy', // e.g., us-east-2_uXboG5pAb
+        userPoolClientId: '41t55oqjkckls4sfcvrsq208co', // e.g., 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: 'q7fb1ur1e46a6a4bgahras87hr9dp4i9a1o8ud8kh53abqnitqg', // Add your Cognito App Client Secret
+        region: 'ap-south-1' // e.g., us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g., https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
     }
 };
