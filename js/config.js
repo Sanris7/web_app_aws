2f6e1dcb210c426c71bb41e65121c722ab3df5f1
Sanris7 <115407290+Sanris7@users.noreply.github.com>
2024-11-24 21:52:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_jmRRPAqWy', // e.g., us-east-2_uXboG5pAb
-        userPoolClientId: '41t55oqjkckls4sfcvrsq208co', // e.g., 25ddkmj4v6hfsfvruhpfi7n4hv
-        clientSecret: 'q7fb1ur1e46a6a4bgahras87hr9dp4i9a1o8ud8kh53abqnitqg', // Add your Cognito App Client Secret
+        userPoolClientId: '4fpmhvmpk5gbt7vakqjod9v402', // e.g., 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g., us-east-2
     },
     api: {
